21ba250f20fe8a0cbb0958e5925309b09555fc42
Ashwin Varghese Kuruttukulam <ashwinvk94@gmail.com>
2019-05-05 23:46:27 -0400

    Add obstacle map generation

--- position_publisher.py
+++ position_publisher.py
@@ -7,21 +7,12 @@ Final Project: Quadcopter Exploration
 Authors:
 Ashwin Varghese Kuruttukulam(ashwinvk94@gmail.com)
 Rachith Prakash (rachithprakash@gmail.com)
-
+Midhun Bharadwaj
 Graduate Students in Robotics,
 University of Maryland, College Park
-'''
 
-#!/usr/bin/env python2
-'''
-Airgilty
+Note: The axis is defined as the x axis down and y axis up with the origin at the bottom left corner
 
-Updates RC commands to avoid collision with objects as detected by the obstacle_detector nodes 
-If an obstacle is detected in the pitch axis, the quadcopter will move in the opposite direction
-for a preset amount of time or until the obstacle is no longer in front. Whichever comes first.
-
-Author:
-Ashwin Varghese Kuruttukulam(ashwinvk94@gmail.com)
 '''
 
 import sys
@@ -33,16 +24,19 @@ import tf.transformations
 from geometry_msgs.msg import Pose
 import numpy as np
 import time
+import cv2
 
 class pos_pub:
 	def __init__(self):
 		print 'started'
 		# Init vrep client
 		vrep.simxFinish(-1)
-		clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
-		if clientID != -1:
+		objectsList = ['Wall1','Wall2','Wall3','Wall4','Wall5','Wall6']
+		resolution = 0.01
+		self.clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
+		if self.clientID != -1:
 			#The quad helper contains function implementations for various quadcopter functions
-			quad_functions = quad_helper.quad_helper(clientID)
+			quad_functions = quad_helper.quad_helper(self.clientID)
 			print('Main Script Started')
 			quad_functions.init_sensors()
 			quad_functions.start_sim()
@@ -58,20 +52,22 @@ class pos_pub:
 
 		#Initializing publishers
 		rospy.Subscriber("/quad_explore/target_position", Pose, self.posCb)
-		# self.rcPub = rospy.Publisher("/mavros/rc/override",OverrideRCIn, queue_size = 10)
+		
+		#Code to shift the origin from the center to the bottom left
+		obstOccMat = self.createObstacleOccupancyMat(objectsList,self.clientID,resolution)
+		err,self.quadObjectHandle = vrep.simxGetObjectHandle(self.clientID,'Quadricopter',vrep.simx_opmode_blocking)
+		err,self.cuboidObjectHandle = vrep.simxGetObjectHandle(self.clientID,'Cuboid',vrep.simx_opmode_blocking)
 
-		while not rospy.is_shutdown() and vrep.simxGetConnectionId(clientID) != -1:
+		while not rospy.is_shutdown() and vrep.simxGetConnectionId(self.clientID) != -1:
 			#Getting object position with respect to first joint
-			# obj_pos = quad_functions.get_obj_pos()
-			# print 'Current Position'
-			# print obj_pos
+			err,obj_pos = vrep.simxGetObjectPosition(self.clientID,self.quadObjectHandle,-1,vrep.simx_opmode_blocking)
 
 			#Setting the position of the quadcopter
 			self.quad_pos = [0,0,3]
-			print 'moving quad'
+			# print 'moving quad'
 			quad_functions.move_quad(self.quad_pos)
 
-			vrep.simxSynchronousTrigger(clientID);
+			vrep.simxSynchronousTrigger(self.clientID);
 			self.rate.sleep()
 		quad_functions.stop_sim()
 	
@@ -82,6 +78,48 @@ class pos_pub:
 		position = data.pose.position
 		self.quad_pos = [position.x,position.y,position.z]
 
+	'''
+	Creates the obstacle space and occupancy matrix
+	0's - Empty Cell
+	1's - Obstacle Cell
+	2's - Visited Cell
+	'''
+	def createObstacleOccupancyMat(self,objectsList,clientID,resolution):
+
+		#Getting origin handle
+		err,originHandle = vrep.simxGetObjectHandle(clientID,'Origin',vrep.simx_opmode_blocking)
+
+		#Getting map size from the top wall and left wall length
+		err,topWallHandle = vrep.simxGetObjectHandle(clientID,'Top_Wall',vrep.simx_opmode_blocking)
+		err,halfTopWallLen = vrep.simxGetObjectFloatParameter(clientID,topWallHandle,18,vrep.simx_opmode_blocking)
+		self.xMapLen = halfTopWallLen*2
+		err,leftWallHandle = vrep.simxGetObjectHandle(clientID,'Left_Wall',vrep.simx_opmode_blocking)
+		err,halfLeftWallLen = vrep.simxGetObjectFloatParameter(clientID,leftWallHandle,19,vrep.simx_opmode_blocking)
+		self.yMapLen = halfLeftWallLen*2
+		obstacleMap = np.zeros((int(self.yMapLen/resolution), int(self.xMapLen/resolution)))
+
+		rectsInfo = []
+		
+		for objectName in objectsList:
+			err,objectHandle = vrep.simxGetObjectHandle(clientID,objectName,vrep.simx_opmode_blocking)
+			err,obj_pos = vrep.simxGetObjectPosition(clientID,objectHandle,originHandle,vrep.simx_opmode_blocking)
+			err,maxX = vrep.simxGetObjectFloatParameter(clientID,objectHandle,18,vrep.simx_opmode_blocking)
+			err,maxY = vrep.simxGetObjectFloatParameter(clientID,objectHandle,19,vrep.simx_opmode_blocking)
+			lengthX = maxX*2
+			lengthY = maxY*2
+			topX = obj_pos[0]-maxX
+			topY = obj_pos[1]+maxY
+			# Rectangles Format[left top corner x,left top corner y,x length,y length]
+			rectsInfo.append([topX,topY,lengthX,lengthY])
+		for x in range(int(self.xMapLen/resolution)):
+			for y in range(int(self.yMapLen/resolution)):
+				# Recangles
+				for rectInfo in rectsInfo:
+					if x>=rectInfo[0]/resolution and y<=rectInfo[1]/resolution and y>=(rectInfo[1]-rectInfo[3])/resolution and x<=(rectInfo[0]+rectInfo[2])/resolution:
+						obstacleMap[y,x] = 1
+		cv2.imshow('obstaclemap',obstacleMap)
+		cv2.waitKey(0)
+		cv2.destroyAllWindows()
 def main(args):
     rospy.init_node('pos_pub', anonymous=True)
     ic = pos_pub()
